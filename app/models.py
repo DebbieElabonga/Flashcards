ad61206acac23584084452a81ab1d3be42025ca8
Benson <bensongathu23@gmail.com>
2021-07-20 21:46:44 +0300

    phase1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    profile_pic = models.ImageField(upload_to='images/',default='SOME IMAGE')
+    bio = models.CharField(max_length=250)
+    contact = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    
+    def save_tag(self):
+        self.save()
+
+
+class Card(models.Model):
+    title = models.CharField(max_length=50)
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    note = models.TextField(max_length=30000)
+    time = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+    def save_article(self):
+        self.save()
+
+    def delete_card(self):
+        self.delete()
+    
+    def update_card(self):
+        pass
+
+
--- flashcards/settings.py
+++ flashcards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+
             ],
         },
     },
@@ -76,8 +79,10 @@ WSGI_APPLICATION = 'flashcards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flashcards',
+        'USER':'ghost',
+        'PASSWORD':'Bkubwa'
     }
 }
 
@@ -124,3 +129,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#.........
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

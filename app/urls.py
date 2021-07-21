32234bbf4b91edcc95754783f60c74f99031c030
Fai <chepngetichfaith46@gmail.com>
2021-07-21 13:26:08 +0300

    add index view function,template to render it and its url and create card view function and its logic.It also creates forms to enable creation of new cards

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('',views.index,name='index'),
+    path('create/',views.createcard,name='createcard'),
     
 ]
 
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from app.models import Card
+from django.shortcuts import render,redirect
+from .forms import CardForm
 
 # Create your views here.
+
+def index(request):
+    cards=Card.objects.all()
+
+    return render(request,'index.html',{'cards':cards})
+
+def createcard(request):
+    if request.method == "POST":
+
+        form=CardForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj=form.instance
+        return redirect('index')
+    else:
+        form=CardForm()
+        card=Card.objects.all()
+    return render(request,"create-card.html",{"form":form,'card':card})

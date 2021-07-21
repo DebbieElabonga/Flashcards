a98700c7aedfd5ced2d1946897c3ecfc5cbef9f1
Fai <chepngetichfaith46@gmail.com>
2021-07-21 20:23:13 +0300

    Update create card view function and form

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from app.models import Card
+from app.models import Card, Subject
 from django.shortcuts import render,redirect
 from .forms import CardForm
 
@@ -10,14 +10,31 @@ def index(request):
     return render(request,'index.html',{'cards':cards})
 
 def createcard(request):
+    subjects=Subject.objects.all()
     if request.method == "POST":
+        data=request.POST
 
-        form=CardForm(data=request.POST,files=request.FILES)
-        if form.is_valid():
-            form.save()
-            obj=form.instance
+        if data['subject']!='none':
+            subject=Subject.objects.get(id=data['subject'])
+        elif data['subject_new']!='':
+            subject,created=Subject.objects.get_or_create(name=data['subject_new'])
+        else:
+            subject=None
+        cards=Card.objects.create(
+            subject=subject,
+            title=data['title'],
+            note=data['note'],
+
+        )
         return redirect('index')
-    else:
-        form=CardForm()
-        card=Card.objects.all()
-    return render(request,"create-card.html",{"form":form,'card':card})
+    context={'subjects':subjects}
+
+    #     form=CardForm(data=request.POST,files=request.FILES)
+    #     if form.is_valid():
+    #         form.save()
+    #         obj=form.instance
+    #     return redirect('index')
+    # else:
+    #     form=CardForm()
+    #     card=Card.objects.all()
+    return render(request,"create-card.html",context)
